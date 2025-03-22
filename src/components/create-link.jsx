873e67fb5053aa91c00ba6f970afc9d5ea529d27
Shambhavi Rajshree <shambhaviiii28@gmail.com>
2025-03-22 17:48:33 +0530

    Updated Features

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -118,7 +118,7 @@ export function CreateLink() {
         />
         {errors.longUrl && <Error message={errors.longUrl} />}
         <div className="flex items-center gap-2">
-          <Card className="p-2">snippy.in</Card> /
+          <Card className="p-2">snippy-beta.vercel.app</Card> /
           <Input
             id="customUrl"
             placeholder="Custom Link (optional)"
--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://snippy.in/${link}`}
+            href={`https://snippy-beta.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://snippy.in/{link}
+            https://snippy-beta.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://snippy.in/${link}`)
+                navigator.clipboard.writeText(`https://snippy-beta.vercel.app/${link}`)
               }
             >
               <Copy />
